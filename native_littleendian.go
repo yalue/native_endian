733eaa7e6783ccb6a50db8f1eff2b3243152e525
yalue <alchohol.rulez@gmail.com>
2021-11-03 08:26:12 -0400

    Add loong64
     - The loong64 spec says that it's little-endian.

--- native_littleendian.go
+++ native_littleendian.go
@@ -1,4 +1,4 @@
-// +build 386 amd64 amd64p32 arm arm64 mipsle mis64le mips64p32le ppc64le riscv riscv64 wasm
+// +build 386 amd64 amd64p32 arm arm64 loong64 mipsle mis64le mips64p32le ppc64le riscv riscv64 wasm
 
 package native_endian
 
